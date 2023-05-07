7986d6701cb9ed031e12f48d09c511e9df022b2f
GJ Tiquia <gershom.tiquia@gmail.com>
2023-05-07 10:51:44 +0800

    fix: home page scroll issue

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import CreateDumpButton from "@/components/BigCreateDumpButton";
 export default function Home() {
 
   return (
-    <section className="min-h-[95vh] flex flex-col items-center justify-center">
+    <section className="min-h-[90vh] flex flex-col items-center justify-center">
       <h1 className="font-bold text-slate-800 text-8xl mb-2">Dump</h1>
       <h2 className="text-xl text-slate-800 mb-8">Dump anything.</h2>
 
